5abbcd4ebc5c36097afccf5f94c85e9f70ee7fe1
MrTrilB <anthony.bluthner@outlook.com>
2026-08-24 10:19:48 +0100

    Add Bootstrap license file and create wpoverride.js

--- src/Assets/Assets.php
+++ src/Assets/Assets.php
@@ -31,6 +31,7 @@ final class Assets {
      */
     public function register(): void {
         add_filter( 'mspress_base_assets', [ $this, 'default_assets' ], 10, 2 );
+        add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_admin' ] );
     }
     /**
      * Registers assets for a specific page.
